dbe6038b8aa4bd7827fc90a9167fd00cbf1ba481
Marc Dilinila <msdilinila@gmail.com>
2020-11-02 15:01:45 -0500

    Setting up basic spring project

---
+++ Pikmin/src/main/java/com/nintendo/Pikmin/PikminApplication.java
@@ -0,0 +1,13 @@
+package com.nintendo.Pikmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PikminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PikminApplication.class, args);
+	}
+
+}
